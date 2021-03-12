8b43d96a61bac0752e1458f3eb703038f9278d5c
Allakot-Harsha <harshamohan023@gmail.com>
2021-03-12 06:12:55 +0530

    js commit

---
+++ movie.js
@@ -0,0 +1,18 @@
+const container=document.querySelector('.container');
+const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+const count=document.getElementById('count');
+const price=document.getElementById('price');
+
+const movieSelect=document.getElementById('movie');
+let tp=+movieSelect.value;
+const populateUI=()=>{
+    const selectedSeats=JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!= null&&selectedSeats.length>0){
+        seats.forEach((seat,index)=>{
+            if(selectedSeats.indexOf(index)>-1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    const s
+}
