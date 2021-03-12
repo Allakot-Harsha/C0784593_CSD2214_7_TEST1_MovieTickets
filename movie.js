6ed5c13c1c3007f338ab613a47652727ccc28876
Allakot-Harsha <harshamohan023@gmail.com>
2021-03-12 08:41:42 +0530

    final commit

--- movie.js
+++ movie.js
@@ -2,9 +2,9 @@ const container=document.querySelector('.container');
 const seats=document.querySelectorAll('.row .seat:not(.occupied)');
 const count=document.getElementById('count');
 const price=document.getElementById('price');
-
+//user selected movie is passed by its Id
 const movieSelect=document.getElementById('movie');
-let tp=+movieSelect.value;
+let tp=+movieSelect.value; //total price calculation
 const populateUI=()=>{
     const selectedSeats=JSON.parse(localStorage.getItem('selectedSeats'));
     if(selectedSeats!= null&&selectedSeats.length>0){
@@ -25,7 +25,7 @@ const populateUI=()=>{
 
     }
 };
-populateUI();
+
 selectedMovie=(movieIndex,moviePrice)=>{
     localStorage.setItem('selectedMovieIndex',movieIndex);
     localStorage.setItem('selectedMoviePrice',moviePrice);
